f498a4fc604b9d89208d308da7cb80dbf69be099
hwclass <harderworking.class@gmail.com>
2016-04-22 22:50:56 +0300

    added service worker

--- browserapi.js
+++ browserapi.js
@@ -18,26 +18,33 @@
 
 var browserapi = (function () {
    
-   let fullScreenApi;
+   const fullScreenApi;
 
    return {
-      getFullScreenApi: (element) => {
-         let instance = null;
-        if(element.requestFullscreen) {
-          instance = element.requestFullscreen();
-        } else if(element.mozRequestFullScreen) {
-          instance = element.mozRequestFullScreen();
-        } else if(element.webkitRequestFullscreen) {
-          instance = element.webkitRequestFullscreen();
-        } else if(element.msRequestFullscreen) {
-          instance = element.msRequestFullscreen();
+      get : {
+        fullScreenApi: (element) => {
+           const instance = null;
+          if(element.requestFullscreen) {
+            instance = element.requestFullscreen();
+          } else if(element.mozRequestFullScreen) {
+            instance = element.mozRequestFullScreen();
+          } else if(element.webkitRequestFullscreen) {
+            instance = element.webkitRequestFullscreen();
+          } else if(element.msRequestFullscreen) {
+            instance = element.msRequestFullscreen();
+          }
+          return instance;
+        },
+        battery: () => {
+           navigator.getBattery().then((battery) => {
+              return battery;
+           })
+        },
+        serviceWorker: () => {
+          if(!!navigator.serviceWorker) {
+            return navigator.serviceWorker;
+          }
         }
-        return instance;
-      },
-      getBattery: () => {
-         navigator.getBattery().then((battery) => {
-            return battery;
-         })
       }
    }   
 
